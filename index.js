c87ce5a2c620907d518478b21aa48ba72aca44f0
migoy96 <142022968+migoy96@users.noreply.github.com>
2023-09-01 22:29:48 +0800

    Add files via upload
    last commit

--- index.js
+++ index.js
@@ -25,3 +25,32 @@ function oztopounds(){
     let num8 = document.getElementById("num8");
     num8.value = result;
 }
+
+function reset(){
+    let num1 = document.getElementById("num1");
+    num1.value = "";
+    let num2 = document.getElementById("num2");
+    num2.value = "";
+}
+
+function reset2(){
+    let num3 = document.getElementById("num3");
+    num3.value = "";
+    let num4 = document.getElementById("num4");
+    num4.value = "";
+}
+
+function reset3(){
+    let num5 = document.getElementById("num5");
+    num5.value = "";
+    let num6 = document.getElementById("num6");
+    num6.value = "";
+}
+
+function reset4(){
+    let num7 = document.getElementById("num7");
+    num7.value = "";
+    let num8 = document.getElementById("num8");
+    num8.value = "";
+}
+    
